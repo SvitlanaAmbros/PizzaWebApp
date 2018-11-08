0eac6e9f35f47f5a51bab7aa0d011ef363dc1935
SvitlanaAmbros <svitlana.ambros@gmail.com>
2018-11-08 13:52:59 +0200

    create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { ProductsComponent } from './products/products.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    ProductsComponent,
+    RestaurantsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
