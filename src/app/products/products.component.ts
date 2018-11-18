a2ab3de8599d9b5cb7a7119704bca0186687bf3e
SvitlanaAmbros <svitlana.ambros@gmail.com>
2018-11-18 16:52:20 +0200

    add setting background image in html

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
   public menu: string[] = ["Пицца", "Десерты", "Напитки"];
 
+  public customImg:string = '0001.JPG';
   constructor() { }
 
   ngOnInit() {
