d6291590282cca85a9da3a85ca48f004d408b9cc
SvitlanaAmbros <svitlana.ambros@gmail.com>
2018-11-20 23:08:32 +0200

    layout fix

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -25,36 +25,5 @@ export class ProductsComponent implements OnInit {
         this.pizzasInfo = data;
       });
     });
-
-    // this.http.get('http://localhost:8080/init').toPromise().then(data => {
-    //   this.http.get('http://localhost:8080/pizzas')
-    //   .subscribe((data: any) => {this.pizzasInfo = data; console.log (this.pizzasInfo); });
-    // }); 
-
-    // this.http.get('http://localhost:8080/init')
-    //   .subscribe( (res) => {
-    //     console.log(res);
-    //     this.http.get('http://localhost:8080/pizzas')
-    //       .subscribe((data: pizzasInfo.db.Pizzas) => {
-    //         console.log(data);
-    //         this.pizzasInfo = data;
-    //       });
-    //   })
-    // let promise  = new Promise ((resolve, reject) => {
-    //   this.http.get('http://localhost:8080/pizzas').subscribe((res) => {
-    //     console.log(res);
-    //   });
-    // })
-
-
-    
-    
-    // this.http.get('http://localhost:8080/pizzas').pipe(map(data => {
-    //   this.pizzasInfo = data;
-    //   console.log(this.pizzasInfo);
-    // }));
-
-
-
   }
 }
