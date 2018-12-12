f46057f5cc149f18098f6546497e124caef74d40
SvitlanaAmbros <svitlana.ambros@gmail.com>
2018-12-12 21:48:19 +0200

    change post method for returning promise

--- src/app/pizzas-info.service.ts
+++ src/app/pizzas-info.service.ts
@@ -36,15 +36,15 @@ export class PizzasInfoService {
     });
   }
 
-  public sendOrderedPizza(order: entity.db.Order) {
-    this.http.post(`${this.URL}purchases`, order)
-      .subscribe((data: {id : string}) => {
-        console.log();
-        alert(`Заказ отправлен! Номер заказа ${data.id}`)
-      });
-
-    // return 
+  public sendOrderedPizza(order: entity.db.Order): Promise<{id : string}>{
+    return new Promise((resolve, reject) => {
+      this.http.post(`${this.URL}purchases`, order)
+        .subscribe((data: {id : string}) => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
   }
-
 
 }
